706f61378a74d7e072e37cfa302882d027ea5d3f
songbaone <namltgt2002@gmail.com>
2026-06-02 16:39:17 +0700

    OK

--- backend/src/sqlquery.sql
+++ backend/src/sqlquery.sql
@@ -1,4 +1,5 @@
-﻿CREATE TABLE Roles
+﻿﻿
+CREATE TABLE Roles
 (
     role_id INT PRIMARY KEY IDENTITY(1,1),
     role_name NVARCHAR(50) NOT NULL UNIQUE,
@@ -14,17 +15,15 @@ CREATE TABLE Users
     email VARCHAR(100) UNIQUE,
     phone VARCHAR(15) UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
     gender NVARCHAR(10),
     birth_date DATE,
     avatar VARCHAR(255),
     status BIT DEFAULT 1,
     created_at DATETIME DEFAULT GETDATE(),
     updated_at DATETIME DEFAULT GETDATE(),
-
     role_id INT NOT NULL,
-
-    CONSTRAINT FK_Users_Roles
-    FOREIGN KEY(role_id)
+    CONSTRAINT FK_Users_Roles FOREIGN KEY(role_id)
     REFERENCES Roles(role_id)
 );
 
@@ -402,3 +401,95 @@ Orders
  └── Payments
 
  */
+
+GO
+
+CREATE TRIGGER TRG_Users_UpdatedAt
+ON Users
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE Users
+    SET updated_at = GETDATE()
+    FROM Users u
+        INNER JOIN inserted i
+        ON u.user_id = i.user_id;
+END;
+GO
+
+CREATE TRIGGER TRG_Categories_UpdatedAt
+ON Categories
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE Categories
+    SET updated_at = GETDATE()
+    FROM Categories c
+        INNER JOIN inserted i
+        ON c.category_id = i.category_id;
+END;
+GO
+
+CREATE TRIGGER TRG_Brands_UpdatedAt
+ON Brands
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE Brands
+    SET updated_at = GETDATE()
+    FROM Brands b
+        INNER JOIN inserted i
+        ON b.brand_id = i.brand_id;
+END;
+GO
+
+CREATE TRIGGER TRG_Products_UpdatedAt
+ON Products
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE Products
+    SET updated_at = GETDATE()
+    FROM Products p
+        INNER JOIN inserted i
+        ON p.product_id = i.product_id;
+END;
+GO
+
+CREATE TRIGGER TRG_ProductVariants_UpdatedAt
+ON ProductVariants
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE ProductVariants
+    SET updated_at = GETDATE()
+    FROM ProductVariants pv
+        INNER JOIN inserted i
+        ON pv.variant_id = i.variant_id;
+END;
+GO
+
+CREATE TRIGGER TRG_Coupons_UpdatedAt
+ON Coupons
+AFTER UPDATE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    UPDATE Coupons
+    SET updated_at = GETDATE()
+    FROM Coupons c
+        INNER JOIN inserted i
+        ON c.coupon_id = i.coupon_id;
+END;
+GO
